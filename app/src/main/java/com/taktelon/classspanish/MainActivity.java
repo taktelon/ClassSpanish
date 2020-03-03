07da473a5b1717f9490062f1062f25d070c02a8c
taktelon <taktelon@gmail.com>
2020-03-02 22:53:50 -0300

    Use Framelayout to set fragment

--- app/src/main/java/com/taktelon/classspanish/MainActivity.java
+++ app/src/main/java/com/taktelon/classspanish/MainActivity.java
@@ -1,6 +1,8 @@
 package com.taktelon.classspanish;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -12,6 +14,15 @@ public class MainActivity extends AppCompatActivity implements VerbFragment.OnLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.addToBackStack(null);
+            int columnCount = 1;
+            fragmentTransaction.replace(R.id.fragment_container, VerbFragment.newInstance(columnCount));
+            fragmentTransaction.commit();
+        }
+
     }
 
     // implements VerbFragment.OnListFragmentInteractionListener
